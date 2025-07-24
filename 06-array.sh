d9c39aa4dc4fa8b2f2fc87b4c9a2455fc14e450b
SrinivasPabballa <srinivassunny64@gmail.com>
2025-07-24 14:01:16 +0530

    opertions

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTillu" "Murari")
+
+echo "First Movi is : ${MOVIES[0]}"
+echo "Second Movie is : ${MOVIES[1]}"
+echo "Third Movie is : ${MOVIES[2]}"
+echo "All Movies are : ${MOVIES[@]}"
