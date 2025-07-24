d1dc5ade5b3f8ceb297a6d92eb1098d963c0c973
SrinivasPabballa <srinivassunny64@gmail.com>
2025-07-24 14:05:36 +0530

    opertions

--- 06-array.sh
+++ 06-array.sh
@@ -2,7 +2,15 @@
 
 MOVIES=("RRR" "DJTillu" "Murari")
 
-echo "First Movi is : ${MOVIES[0]}"
+echo "First Movie is : ${MOVIES[0]}"
 echo "Second Movie is : ${MOVIES[1]}"
 echo "Third Movie is : ${MOVIES[2]}"
 echo "All Movies are : ${MOVIES[@]}"
+
+
+CARS=("BMW" "BENZ" "AUDI")
+
+echo "First Car is : ${CARS[0]}"
+echo "Second Car is : ${CARS[1]}"
+echo "Third Car is : ${CARS[2]}"
+echo "All cars are : ${CARS[@]}"
