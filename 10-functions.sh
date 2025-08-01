5a1729d854f5bcc7e91ed8ea4f3bf1a1680951cd
SrinivasPabballa <srinivassunny64@gmail.com>
2025-08-01 17:31:48 +0530

    Functions

--- 10-functions.sh
+++ 10-functions.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." F1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE() {
  
- if [ $? -ne 0 ]
+ if [ $1 -ne 0 ]
  then 
  echo "$2... FAILURE"
  exit 1
@@ -20,10 +23,10 @@ then
     exit 1 
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
  
 VALIDATE $? "Installing MySql"
 
-dnf install gitt -y
+dnf install git -y &>>$LOGFILE
 
 VALIDATE $? "Installing Git"
