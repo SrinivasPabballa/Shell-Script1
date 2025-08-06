ab5366e3d82d1ad4024841dfa67421172df0efa4
SrinivasPabballa <srinivassunny64@gmail.com>
2025-08-06 14:46:38 +0530

    functions

---
+++ 11-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="/e[31m"
+G="/e[32m"
+N="/e[0m"
+
+VALIDATE() {
+ 
+ if [ $1 -ne 0 ]
+ then 
+ echo -e "$2...$R FAILURE $N"
+ exit 1
+
+ else
+ echo -e "$2...$G SUCCESS $N"
+ fi
+}
+
+if [ $USERID -ne 0 ]
+
+then 
+    echo "Please proceed with superuser"
+    exit 1 
+fi
+
+dnf install mysql -y  &>>$LOGFILE
+ 
+VALIDATE $? "Installing MySql"
+
+dnf install git -y  &>>$LOGFILE
+
+VALIDATE $? "Installing Git"
+
+dnf install dockerr -y &>>LOGFILE
+
+VALIDATE $? "Installating docker"
+
