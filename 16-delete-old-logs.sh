e3f014f521c48c4898339f9681162b012e089cf9
SrinivasPabballa <srinivassunny64@gmail.com>
2025-09-04 16:16:49 +0530

    deleting older logs

---
+++ 16-delete-old-logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app_logs
+ 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m" 
+
+if [ -d $SOURCE_DIRECTORY ]
+then 
+    echo -e  " $G source directory exists $N"
+else 
+    echo -e " $R please make sure $SOURCE_DIRECTORY exists $N" 
+fi    
+
+FILES=$( find $SOURCE_DIRECTORY -name "*.log" -mtime +14 )
+
+while IFS=read -r line 
+do 
+  echo "deleting file : $line"
+  rm -rf $line 
+done <<< $FILES 
